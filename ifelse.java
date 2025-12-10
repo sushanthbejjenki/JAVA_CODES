cb07c5768d982bc1f1c160b0183662339701642f
sushanthbejjenki <sushanthbejjenki@gmail.com>
2025-12-10 16:15:22 +0530

    Add files via upload
    IF ELSE code for begginers

---
+++ ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int age = 25; 
+        if (age < 18) {
+            System.out.println("You are a minor.");
+        } 
+        else {
+            System.out.println("You are an major.");
+        }
+    }
+}
