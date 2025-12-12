da1788e7b907162a5a112c35d5681ac44131cf25
sushanth bejjenki <sushanthbejjenki@gmail.com>
2025-12-12 09:44:41 +0530

    try catch error tutorial for arithmetic  error

---
+++ trycatch.java
@@ -0,0 +1,16 @@
+public class trycatch {
+    public static void main(String[] args) {
+        int a= 10;
+        int b=0;
+        try {
+            int c=a/b;
+            System.out.println("result : " +c);
+        }
+        catch (Exception e) {
+            System.out.println("Exception is : " + e.getMessage());
+
+        }
+        }
+    }
+    
+
